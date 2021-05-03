241caf13d578e75ec78b794bcc5618912125438d
msunilhyd <msunilhyd@gmail.com>
2021-05-02 23:22:40 -0500

    02-May-21 : Sun : Added OddNumbers java vode

---
+++ dailydose/src/com/dailydose/may/OddNumbers.java
@@ -0,0 +1,11 @@
+package com.dailydose.may;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		for (int i=1; i< 10; i++) {
+			System.out.println(i);
+			i = i + 1;
+		}
+	}
+}

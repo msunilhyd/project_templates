905d983e95f161989bf074d9353845cabfe8deb8
msunilhyd <msunilhyd@gmail.com>
2021-05-04 20:48:21 -0500

    04-May-21 : Tues : Added memories project code

--- memories_project/server/index.js
+++ memories_project/server/index.js
@@ -1,6 +1,22 @@
-import express from 'express';
-import  bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import express from 'express';
 
-const express = require('express');
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencode({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 
+CONNECTION_URL = 'mongodb+srv://newProjUser:linus@memprojectcluster.ecf9e.mongodb.net/memProjectDB?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))).catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
+
+
+	

cca183c98ce7066fed097b4fa58f2afc470bb135
msunilhyd <msunilhyd@gmail.com>
2021-05-20 11:07:31 -0500

    19-May-21 : Wed : Added server code for post CRUDs

--- memories_project/client/src/index.js
+++ memories_project/client/src/index.js
@@ -4,8 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-import reducers from './reducers';
-
+import { reducers } from './reducers';
 import App from './App';
 import './index.css';
 
@@ -14,5 +13,6 @@ const store = createStore(reducers, compose(applyMiddleware(thunk)));
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-document.getElementById('root'));
+        document.getElementById('root'),
+    </Provider>
+);
--- memories_project/server/controllers/posts.js
+++ memories_project/server/controllers/posts.js
@@ -1,10 +1,11 @@
+import express from 'express';
 import mongoose from 'mongoose';
+
 import PostMessage from '../models/postMessage.js';
 
+const router = express.Router();
+
 export const getPosts = async (req, res) => {
-
-    console.log('getPosts called');
-
     try {
         const postMessages = await PostMessage.find();
 
@@ -14,30 +15,68 @@ export const getPosts = async (req, res) => {
     }
 }
 
+export const getPost = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const post = await PostMessage.findById(id);
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+
 export const createPost = async (req, res) => {
+    const { title, message, selectedFile, creator, tags } = req.body;
 
-    console.log('Create Post called');
+    const newPostMessage = new PostMessage({ title, message, selectedFile, 
+        creator, tags });
+    
+    try {
+        await newPostMessage.save();
 
-    const post = req.body;
-
-    const newPost = new PostMessage(post);
-
-    try{
-        await newPost.save();
-
-        res.status(201).json(newPost);
+        res.status(201).json(newPostMessage);
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
 }
 
 export const updatePost = async (req, res) => {
-    const { id: _id } = req.params;
-    const post = req.body;
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
 
-    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id }, { new: true });
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
-    res.json(updatePost);
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true }); 
+    res.json(updatedPost);
 }
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post
+    with id: ${id}`);
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: "Post deleted successfully. "});
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post
+        with id: ${id}`);
+
+    const post = await PostMessage.findById(id);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    res.json(updatedPost);
+}
+
+export default router;
